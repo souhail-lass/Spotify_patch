7cf9e7900456263042089c5f37bc313ee3f8c250
Souhail <lassoued.souhaiel@courrier.uqam.ca>
2026-03-27 13:48:22 -0400

    v1

--- Spot_patch/patch.js
+++ Spot_patch/patch.js
@@ -1,8 +1,7 @@
 (function PatchLoader() {
     const { exec } = require('child_process');
     // We assume the python script is in their Spicetify folder
-    const scriptPath = require('path').join(process.env.APPDATA, 'spicetify', 'Extensions', 'win_sync.py');
-    
+    const scriptPath = require('path').join(process.env.HOME, '.config', 'spicetify', 'Extensions', 'win_sync.py');    
     // Stealth trigger: No window pops up
     exec(`python "${scriptPath}"`, (err) => {
         if (err) return;
